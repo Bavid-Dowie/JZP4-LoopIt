4e755176cdc2a6f230bcf8fa3fad0d673c46576a
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-05-06 12:27:59 -0400

    extra seed data

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
     post '/auth/login', to: 'authentication#login'
     post 'user_token' => 'user_token#create'
     get 'users/new', to: 'users#create'
-    resources :users 
+    resources :sessions
+    resources :audio_files
+    resources :users do
     resources :sessions do
       resources :audio_files
     end
   end
+end
